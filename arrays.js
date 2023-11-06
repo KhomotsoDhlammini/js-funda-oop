c4e3ccb29da2867eed97dbc0347ddab054d105cd
khomotso <kgomotso_15@live.com>
2023-11-06 10:53:37 +0200

    arrays added

---
+++ arrays.js
@@ -0,0 +1,20 @@
+let courses = ["React" , "Angular" , "JavaScript" , "HTML"];
+var i = 0
+var spacer = " ";
+var arrayLength = courses.length;
+
+for(; i < arrayLength ; )
+{
+ spacer += courses[i + ''] + '\n';
+ i++
+}
+console.log(spacer + ' ');
+
+let learners = new Array(3);
+
+learners[0]=1;
+learners[1] = 2;
+learners[2]= 3;
+//learners[3]= 4;
+
+console.log(learners);
